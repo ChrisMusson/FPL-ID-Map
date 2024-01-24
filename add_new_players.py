042e4557f6a7824fa026f7fe8c195939ce8c0b29
Chris Musson <Chris.Musson@hotmail.com>
2024-01-24 18:17:58 +0000

    Upload scripts for updating CSV files

---
+++ add_new_players.py
@@ -0,0 +1,31 @@
+import csv
+import os
+import requests
+
+
+def append_new_to_existing(filepath, new):
+    with open(filepath, "a", encoding="utf-8", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(new)
+
+
+with open(os.path.join("FBRef.csv"), "r", encoding="utf-8") as f:
+    data = [row for row in csv.reader(f)][1:]
+    old_codes = set([int(row[0]) for row in data])
+
+with requests.Session() as s:
+    url = "https://fantasy.premierleague.com/api/bootstrap-static/"
+    players = s.get(url).json()["elements"]
+
+data = [["code", "first_name", "second_name", "web_name", "23-24"]]
+for p in players:
+    data.append([p["code"], p["first_name"], p["second_name"], p["web_name"], p["id"]])
+data = [data[0]] + sorted(data[1:], key=lambda x: x[-1])
+
+with open(os.path.join("FPL", "23-24.csv"), "w", encoding="utf-8", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(data)
+
+new_players = [x[:-1] + [None] for x in data[1:] if int(x[0]) not in old_codes]
+for file in ["FBRef.csv", "Understat.csv", "Transfermarkt.csv"]:
+    append_new_to_existing(file, new_players)
---
+++ generate_master.py
@@ -0,0 +1,24 @@
+import os
+import pandas as pd
+
+seasons = [f"{season}-{season+1}" for season in range(16, 24)]
+fpl_files = [os.path.join("FPL", f"{season}.csv") for season in seasons]
+files = fpl_files + ["FBRef.csv", "Understat.csv", "Transfermarkt.csv"]
+print(files)
+
+master = pd.read_csv(files[0], index_col="code")
+for file in files[1:]:
+    df = pd.read_csv(file, index_col="code")
+    master = df.combine_first(master)
+
+master = master.sort_values(by="code")
+col_order = (
+    ["first_name", "second_name", "web_name"]
+    + seasons
+    + ["fbref", "understat", "transfermarkt"]
+)
+
+master = master[col_order]
+for col in seasons + ["understat", "transfermarkt"]:
+    master[col] = master[col].astype("Int64")
+master.to_csv("Master.csv", encoding="utf-8")
