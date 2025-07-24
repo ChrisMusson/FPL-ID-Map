c0be75d73027a4c261dc23a3f8fc153e9e82f9aa
Chris Musson <chris.musson@hotmail.com>
2025-07-24 12:58:01 +0100

    Add basic support for fotmob IDs
    A majority of players have their fotmob ids, but I will need to add more to make it complete

--- generate_master.py
+++ generate_master.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 seasons = [f"{season}-{season + 1}" for season in range(16, 26)]
 fpl_files = [os.path.join("FPL", f"{season}.csv") for season in seasons[::-1]]
-files = fpl_files + ["FBRef.csv", "Understat.csv", "Transfermarkt.csv"]
+files = fpl_files + ["FBRef.csv", "Understat.csv", "Transfermarkt.csv", "FotMob.csv"]
 
 master = pd.read_csv(files[0], index_col="code")
 for file in files[1:]:
@@ -12,9 +12,9 @@ for file in files[1:]:
     master = master.combine_first(df)
 
 master = master.sort_values(by="code")
-col_order = ["first_name", "second_name", "web_name"] + seasons + ["fbref", "understat", "transfermarkt"]
+col_order = ["first_name", "second_name", "web_name"] + seasons + ["fbref", "understat", "transfermarkt", "fotmob"]
 
 master = master[col_order]
-for col in seasons + ["understat", "transfermarkt"]:
+for col in seasons + ["understat", "transfermarkt", "fotmob"]:
     master[col] = master[col].astype("Int64")
 master.to_csv("Master.csv", encoding="utf-8")
