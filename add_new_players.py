5ee697b83d94c1e177bad74c2791faf59480d004
Chris Musson <chris.musson@hotmail.com>
2025-07-24 17:09:49 +0100

    Update add_new_players to append new players to FotMob.csv

--- add_new_players.py
+++ add_new_players.py
@@ -30,5 +30,5 @@ with open(os.path.join("FPL", f"{SEASON}.csv"), "w", encoding="utf-8", newline="
     writer.writerows(data)
 
 new_players = [x[:-1] + [None] for x in data[1:] if int(x[0]) not in old_codes]
-for file in ["FBRef.csv", "Understat.csv", "Transfermarkt.csv"]:
+for file in ["FBRef.csv", "Understat.csv", "Transfermarkt.csv", "FotMob.csv"]:
     append_new_to_existing(file, new_players)
