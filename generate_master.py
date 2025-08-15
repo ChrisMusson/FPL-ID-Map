e745e9aac7b32ae884ec15216ac9a3504c534b36
Chris Musson <chris.musson@hotmail.com>
2025-08-15 03:27:51 +0100

    Remove fotmob from id map
    Probably a temporary measure for now as there were some errors. However, the fotmob ids aren't actually very necessary - in most fotmob api endpoints, players will have their fotmob id and also their opta id, which can then be matched with the "code" column in Master.csv

--- generate_master.py
+++ generate_master.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 seasons = [f"{season}-{season + 1}" for season in range(16, 26)]
 fpl_files = [os.path.join("FPL", f"{season}.csv") for season in seasons[::-1]]
-files = fpl_files + ["FBRef.csv", "Understat.csv", "Transfermarkt.csv", "FotMob.csv"]
+files = fpl_files + ["FBRef.csv", "Understat.csv", "Transfermarkt.csv"]
 
 master = pd.read_csv(files[0], index_col="code")
 for file in files[1:]:
@@ -12,9 +12,9 @@ for file in files[1:]:
     master = master.combine_first(df)
 
 master = master.sort_values(by="code")
-col_order = ["first_name", "second_name", "web_name"] + seasons + ["fbref", "understat", "transfermarkt", "fotmob"]
+col_order = ["first_name", "second_name", "web_name"] + seasons + ["fbref", "understat", "transfermarkt"]
 
 master = master[col_order]
-for col in seasons + ["understat", "transfermarkt", "fotmob"]:
+for col in seasons + ["understat", "transfermarkt"]:
     master[col] = master[col].astype("Int64")
 master.to_csv("Master.csv", encoding="utf-8")
