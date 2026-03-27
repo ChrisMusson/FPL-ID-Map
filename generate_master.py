e48ed313f8939910a7d92ccaa5e49daad9d7bf14
Chris Musson <chris.musson@hotmail.com>
2026-03-27 15:24:56 +0000

    Add ability for generate_master.py to update datasource names
    Previously, all names for each datasource file would stay the same even when they have been updated in FPL. Since generate_master.py already calculates the most up-to-date names and uses them, it makes sense to also update the datasource names too.

--- generate_master.py
+++ generate_master.py
@@ -4,17 +4,25 @@ import pandas as pd
 
 seasons = [f"{season}-{season + 1}" for season in range(16, 26)]
 fpl_files = [os.path.join("FPL", f"{season}.csv") for season in seasons[::-1]]
-files = fpl_files + ["FBRef.csv", "Understat.csv", "Transfermarkt.csv", "Whoscored.csv"]
+datasource_files = ["FBRef.csv", "Understat.csv", "Transfermarkt.csv", "Whoscored.csv"]
+files = fpl_files + datasource_files
+name_columns = ["first_name", "second_name", "web_name"]
 
 master = pd.read_csv(files[0], index_col="code")
 for file in files[1:]:
     df = pd.read_csv(file, index_col="code")
     master = master.combine_first(df)
 
-master = master.sort_values(by="code")
-col_order = ["first_name", "second_name", "web_name"] + seasons + ["fbref", "understat", "transfermarkt", "whoscored"]
+master = master.sort_index()
+master_names = master[name_columns]
+col_order = name_columns + seasons + ["fbref", "understat", "transfermarkt", "whoscored"]
 
 master = master[col_order]
 for col in seasons + ["understat", "transfermarkt", "whoscored"]:
     master[col] = master[col].astype("Int64")
-master.to_csv("Master.csv", encoding="utf-8")
+master.to_csv("Master.csv", encoding="utf-8-sig")
+
+for file in datasource_files:
+    df = pd.read_csv(file).set_index("code")
+    df.update(master_names)
+    df.to_csv(file, encoding="utf-8-sig")
