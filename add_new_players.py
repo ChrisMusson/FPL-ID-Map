87f63859726c555cacd1cb329269d9c6dd9ca01b
Chris Musson <chris.musson@hotmail.com>
2026-06-15 14:57:36 +0100

    Update add_new_players to update WhoScored CSV

--- add_new_players.py
+++ add_new_players.py
@@ -38,5 +38,5 @@ with open(os.path.join("FPL", f"{SEASON}.csv"), "w", encoding="utf-8", newline="
     writer.writerows(data)
 
 new_players = [x[:-1] + [None] for x in data[1:] if int(x[0]) not in old_codes]
-for file in ["FBRef.csv", "Understat.csv", "Transfermarkt.csv", "FotMob.csv"]:
+for file in ["FBRef.csv", "Understat.csv", "Transfermarkt.csv", "WhoScored.csv"]:
     append_new_to_existing(file, new_players)
